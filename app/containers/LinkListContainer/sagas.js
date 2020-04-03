b25de11f9ba4a700ff5af4892a38c56bc10772b0
Steven Smith <steven.76484@gmail.com>
2020-04-02 22:18:31 -0400

    Adding LinkList sagas

--- app/containers/LinkListContainer/sagas.js
+++ app/containers/LinkListContainer/sagas.js
@@ -1,8 +1,25 @@
 // import { take, call, put, select } from 'redux-saga/effects';
+import { call } from 'redux-saga/effects';
+import { takeLatest } from 'redux-saga';
+import { SELECT_TOPIC } from '../NavigationContainer/constants';
 
-// Individual exports for testing
+function fetchLinksFromServer(topic) {
+  return fetch(`http://localhost:3000/api/topics/${topic.name}/links`)
+    .then(response => response.json());
+}
+
+export function* fetchLinks(action) {
+  try {
+    const links = yield call(fetchLinksFromServer, action.topic);
+    // TODO: dispatch action to store links
+    console.log('LINKS FROM SERVER:', links);
+  } catch (e) {
+    // TODO: dispatch action to store error
+  }
+}
+
 export function* defaultSaga() {
-  return;
+  yield* takeLatest(SELECT_TOPIC, fetchLinks);
 }
 
 // All sagas to be loaded
